529b95b728ff34993383ad5ec12589032b2c7dd6
Michael Angerman <stormasm@gmail.com>
2021-05-19 10:39:01 -0700

    update getfun and getquote so that they now properly delete the file before writing a new one

--- v00/getfun.py
+++ v00/getfun.py
@@ -1,6 +1,8 @@
-import datetime
-import os
 import argparse
+import datetime
+import glob
+import os
+
 from me.stock_info import get_stats
 from bluemesa.util import lineutil
 from bluemesa.redis import util
@@ -9,19 +11,30 @@ from bluemesa.redis import util
 ## Just enter a filename without the .txt extension
 ## from the directory symbol_dir
 
-path = os.environ['BMTOP']
+path = os.environ["BMTOP"]
 
 ## This is the directory where symbol files are located
-symbol_dir  = path + '/bluemesa/config/symbols/'
+symbol_dir = path + "/bluemesa/config/symbols/"
 
 ## This is the base directory of where files get written to
-base_pathout = path + '/bluemesa/tmp/fun/in/'
+base_pathout = path + "/bluemesa/tmp/fun/in/"
+
+
+def get_and_delete_symbol_files(path, symbol):
+    filenames = []
+    pattern = path + "/" + symbol + "-fun-*.csv"
+    for name in glob.glob(pattern):
+        filenames.append(name)
+    for filename in filenames:
+        if os.path.exists(filename):
+            os.remove(filename)
+
 
 def mkdir_ifnothere(parent_dir, dirname):
     # check to see that parent_dir exists
     # and if does not print problem and exit
     mybool = os.path.isdir(parent_dir)
-    if (not mybool):
+    if not mybool:
         print("mkdir_ifnothere: parent directory does not exist exiting process")
         exit()
     # Path
@@ -29,31 +42,36 @@ def mkdir_ifnothere(parent_dir, dirname):
     mybool = os.path.isdir(path)
     # check to see if dirname exists
     # and if it does then do not create a new directory
-    if (not mybool):
+    if not mybool:
         os.mkdir(path)
-    path = path + '/'
-    return(path)
+    path = path + "/"
+    return path
+
 
 def get_day():
     x = datetime.datetime.now()
     y = x.strftime("%y-%m-%d")
     return y
 
+
 def build_file_name(symbol):
     day = get_day()
     filename = f"{symbol}-fun-{day}.csv"
-    return(filename)
+    return filename
 
-def process(symbols,path,key):
+
+def process(symbols, path, key):
     for symbol in symbols:
+        get_and_delete_symbol_files(path, symbol)
         filename = build_file_name(symbol)
-        bool = util.redis_set_read(key,symbol)
+        bool = util.redis_set_read(key, symbol)
         if not bool:
             print(symbol)
             data = get_stats(symbol)
             out_file = path + filename
             data.to_csv(out_file)
-            #util.redis_set_write(key,symbol)
+            # util.redis_set_write(key,symbol)
+
 
 if __name__ == "__main__":
 
@@ -61,15 +79,15 @@ if __name__ == "__main__":
     parser.add_argument("groupname")
     args = parser.parse_args()
     groupname = args.groupname
-    filename = groupname + '.txt'
+    filename = groupname + ".txt"
 
     # This is the actual symbol file name
     symbol_file = symbol_dir + filename
 
     symbols = lineutil.get_lines_as_set(symbol_file)
-    path_out = mkdir_ifnothere(base_pathout,groupname)
+    path_out = mkdir_ifnothere(base_pathout, groupname)
     redis_check_key = "symbol-check-" + groupname
-    process(symbols,path_out,redis_check_key)
+    process(symbols, path_out, redis_check_key)
 
 # The variable symbols is always a Python set which is nice
 # because then we will not get any duplication of data
